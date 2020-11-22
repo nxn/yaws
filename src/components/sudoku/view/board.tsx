16ad1196a585567778073c61f13a3c2f1925aac4
nxn <ernie@nxn.io>
2020-11-21 23:06:10 -0500

    starts styling implementation using 'styled'

--- src/components/sudoku/view/board.tsx
+++ src/components/sudoku/view/board.tsx
@@ -7,10 +7,15 @@ import React from 'react';
 
 
 import { partialEq } from '@components/utilities/misc';
+import { experimentalStyled as styled } from '@material-ui/core/styles';
+import clsx from 'clsx';
+
 
 type BoardProperties = { 
     model:      IBoard,
-    controller: IBoardController
+    controller: IBoardController,
+    scale:      number,
+    className?:  string
 };
 
 type BoardState = {
@@ -21,7 +26,7 @@ type BoardState = {
     cursorListener?:    IEventListenerKey
 };
 
-export default class Board extends React.Component<BoardProperties, BoardState> {
+class Board extends React.Component<BoardProperties, BoardState> {
     constructor(props: BoardProperties) {
         super(props);
         this.state = {
@@ -102,7 +107,7 @@ export default class Board extends React.Component<BoardProperties, BoardState> 
     render() {
         return (
             <div id={this.props.model.id} 
-                className={ this.state.isReady ? "board" : "board loading" }
+                className={ clsx(this.props.className, { "loading": !this.state.isReady }) }//this.state.isReady ? "board" : "board loading" }
                 onMouseLeave={ this.resetHighlight }>{
                 this.props.model.cells.map((cell: ICell) => 
                     <Cell 
@@ -118,3 +123,95 @@ export default class Board extends React.Component<BoardProperties, BoardState> 
         );
     }
 }
+
+export default styled(Board)(
+    ({theme, scale}) => ({
+        cursor:                 "default",
+        WebkitUserSelect:       'none',
+        MozUserSelect:          'none',
+        msUserSelect:           'none',
+        userSelect:             'none',
+        touchAction:            'auto',
+
+        flexGrow:               0,
+        textAlign:              'center',
+        display:                'grid',
+        gridTemplateColumns:    'repeat(9, auto)',
+        verticalAlign:          'middle',
+
+        backgroundColor:        theme.palette.divider,
+        border:                 `${ 0.1875 * scale }rem solid ${ theme.palette.action.hover }`,
+        borderRadius:           `${ 0.25 * scale }rem`,
+
+        '& .cell': {
+            position:           'relative',
+            backgroundColor:    theme.palette.background.paper,
+            width:              `${ 2.0 * scale }rem`,
+            height:             `${ 2.0 * scale }rem`,
+            marginRight:        '0.0625rem',
+            marginBottom:       '0.0625rem',
+            transition:         `background-color ${ theme.transitions.duration.short }ms ease`,
+            '& > div':          { position: 'absolute' },
+        
+            '& .value': {
+                color:      theme.palette.text.primary,
+                fontFamily: '"Cabin Condensed", sans-serif',
+                fontSize:   `${ 1.3125 * scale }rem`,
+                lineHeight: `${ 2.0000 * scale }rem`,
+                width:      '100%',
+                height:     '100%'
+            },
+        
+            '& .invalid': {
+                color: `${ theme.palette.error.dark }`
+            },
+        
+            '& .notes': {
+                display: 'none'
+            },
+            
+            '&.c1.r1': { borderTopLeftRadius:        `${ 0.125 * scale }rem` },
+            '&.c9.r1': { borderTopRightRadius:       `${ 0.125 * scale }rem` },
+            '&.c1.r9': { borderBottomLeftRadius:     `${ 0.125 * scale }rem` },
+            '&.c9.r9': { borderBottomRightRadius:    `${ 0.125 * scale }rem` },
+
+            /* Separate grid boxes (columns and rows 3 and 6) */
+            '&.c3, &.c6': { marginRight:    `${ 0.1875 * scale }rem` },
+            '&.r3, &.r6': { marginBottom:   `${ 0.1875 * scale }rem` },
+
+            /* Remove borders and margins on the last column and row */
+            '&.c9': { marginRight:     0, borderRight:    0 },
+            '&.r9': { marginBottom:    0, borderBottom:   0 }
+        },
+
+        '& .cell.static': {
+            '& .value': {
+                boxSizing:          'border-box',
+                border:             `0.0625rem solid ${ theme.palette.action.selected }`,
+                backgroundColor:    `${ theme.palette.action.disabledBackground }`,
+                borderRadius:       `${ 0.125 * scale }rem`,
+                lineHeight:         `${ 1.375 * scale }rem`,
+                width:              `${ 1.500 * scale }rem`,
+                height:             `${ 1.500 * scale }rem`,
+                margin:             `${ 0.250 * scale }rem`
+            },
+    
+            '& .invalid': {
+                color:              theme.palette.text.primary,
+                backgroundColor:    `${ theme.palette.error.light }`,
+                borderColor:        `${ theme.palette.error.dark }`,
+                borderWidth:        `${ 0.0625 * scale }rem`,
+                lineHeight:         `${ 1.3125 * scale }rem`
+            }
+        },
+
+        '& .cell.highlight': {
+            backgroundColor:    `${ theme.palette.action.hover }`,
+            transition:         `background-color ${ theme.transitions.duration.short }ms ease`
+        },
+    
+        '& .cell.cursor': {
+            backgroundColor: `${ theme.palette.action.selected }`
+        },
+    })
+);
